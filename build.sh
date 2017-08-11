80c6b6458460a163cebd828223de1989f96de95c
Nate Turner <nater1992@gmail.com>
2017-08-11 13:12:18 -0700

    clean up production config as soon as possible

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ aws s3 cp s3://mymoneymanager-config/parameters.yml ./parameters-production.yml
 # Build php-fpm docker container
 docker build --file docker/php/Dockerfile -t mymoneymanager_php:latest .
 
+# For security reasons, clean up production configuration
+rm ./parameters-production.yml
+
 # Build nginx docker container
 docker build --file docker/nginx/Dockerfile -t mymoneymanager_nginx:latest .
 
@@ -15,6 +18,3 @@ docker tag mymoneymanager_php:latest 964400098929.dkr.ecr.us-west-2.amazonaws.co
 docker tag mymoneymanager_nginx:latest 964400098929.dkr.ecr.us-west-2.amazonaws.com/mymoneymanager_nginx:latest
 docker push 964400098929.dkr.ecr.us-west-2.amazonaws.com/mymoneymanager_php:latest
 docker push 964400098929.dkr.ecr.us-west-2.amazonaws.com/mymoneymanager_php:latest
-
-# For security reasons, clean up production configuration
-rm ./parameters-production.yml
